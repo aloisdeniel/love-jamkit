5c9c99827c8d8249417782c215986346a39538be
Aloïs Deniel <alois.deniel@gmail.com>
2016-02-06 17:51:51 +0100

    Started graphics system rework.

--- jamkit/core/systems/graphics.lua
+++ jamkit/core/systems/graphics.lua
@@ -16,46 +16,19 @@ end
 -- Resolve resource and draws entities to screen from the available entity components
 function Graphics:drawEntity(entity)
   
-  local x,y = entity:getAbsolutePosition()
-  local sx,sy = entity:getAbsoluteScale()
-  local ox,oy = entity.components.origin.x,entity.components.origin.y
-  local r = entity:getAbsoluteRotation()
-  local cr,cg,cb,ca = entity:getAbsoluteColor()
+  local x,y,angle,r,g,b,a,sx,sy = getAbsoluteValues(entity)
   
-  if entity.components.image then
-    local image = self.resources:image(entity.components.image)
-    love.graphics.setColor(cr,cg,cb,ca)
-    if entity.components.animation then    
-      local animation = self.resources.animations[entity.components.animation]
-      local w,h = animation:getDimensions()
-      x = x - (w/2) * sx
-      y = y - (h/2) * sy
-      animation:draw(image, x,y, r, sx, sy, ox, oy)
-    elseif entity.components.quad then
-      local quad = self.assets.quads[entity.components.quad]
-      local _,_,w,h = quad:getViewport()
-      x = x - (w/2) * sx
-      y = y - (h/2) * sy
-      love.graphics.draw(image, quad, x,y,r,sx,sy,ox,oy)
-    else
-      local w,h = image:getDimensions()
-      x = x - (w/2) * sx
-      y = y - (h/2) * sy
-      love.graphics.draw(image, quad, x,y,r,sx,sy,ox,oy)
-    end
-  elseif entity.components.text
-    local text = self.assets.texts[entity.components.text]
-    local w,h = text:getWidth(), text:getHeight()
+  if entity.components.sprite then
+    local image, quad = entity.components.sprite:getImage()
+    local _,_,w,h = quad:getViewport()
     x = x - (w/2) * sx
     y = y - (h/2) * sy
-    love.graphics.draw(text, x,y,r,sx,sy,ox,oy)
-  elseif entity.components.rectangle
-    local rect = entity.components.rectangle
-    love.graphics.draw(rect.mode or "fill",rect.x,rect.y,rect.w,rect.h)
+    love.graphics.setColor(r,g,b,a)
+    love.graphics.draw(image, quad, x,y,angle,sx,sy)
   end
 end
 
--- Gets all absolutes components from parents hierarchy
+-- Gets all absolutes components values from parents hierarchy
 local function getAbsoluteValues(entity)
   local x,y = (entity.components.position and entity.components.position:get()) or (0,0)
   local angle = (entity.components.rotation and entity.components.rotation:get()) or (0)
